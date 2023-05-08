6d1f7ee4d3ebd04a1a00b6bc6ea6ab9b39bc8cd8
itzzrony <132839937+itzzrony@users.noreply.github.com>
2023-05-08 21:42:20 +0600

    Create Madlib Game.cs

---
+++ Madlib Game.cs
@@ -0,0 +1,14 @@
+string color, device, celebrity;
+
+Console.Write("Enter a color: ");
+color = Console.ReadLine();
+
+Console.Write("Enter a device: ");
+device = Console.ReadLine();
+
+Console.Write("Enter a clebrity: ");
+celebrity = Console.ReadLine();
+
+Console.WriteLine("Roses are " + color);
+Console.WriteLine(device + " are violets");
+Console.WriteLine("I love " + celebrity);
